ce4b71e9c71bff50fd6df6759dab17597f6fa0b8
oktopriima <octoprima93@gmail.com>
2020-10-30 01:13:50 +0700

    Clear cached

--- generate-model.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "::: starting :::"
-
-cd domain/core
-
-echo "::: starting generate :::"
-gen --connstr "root:empalgoreng@tcp(127.0.0.1:3306)/mkproject?&parseTime=True" --database mkproject --json
-
-echo "::: finish :::"
